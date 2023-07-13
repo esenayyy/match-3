7c32b081b5da0d5a5494af2edcfd4f5b39af51ca
esenayyy <senaycagdas@hotmail.com>
2023-07-13 18:34:45 +0300

    Initial commit

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Gem : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector2Int posIndex;
+    [HideInInspector]
+    public Board board;
+
+
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+
+    private bool mousePressed;
+    private float SwipeAngle = 0;
+
+    private Gem otherGem;
+
+    public enum GemType { blue, green, red, yellow, purple, bomb}
+    public GemType type;
+
+    public bool isMatched;
+    
+    private Vector2Int previousPos;
+
+    public GameObject destroyEffect;
+
+    public int blastSize = 2;
+
+    public int scoreValue = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Vector2.Distance(transform.position, posIndex) > .01f)
+        {
+            transform.position = Vector2.Lerp(transform.position, posIndex, board.gemSpeed * Time.fixedDeltaTime);
+        } else
+        {
+            transform.position = new Vector3(posIndex.x, posIndex.y, 0f);
+            board.allGems[posIndex.x, posIndex.y] = this;
+        }
+       
+
+        if(mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+
+            if(board.currentState == Board.BoardState.move && board.roundMan.roundTime > 0)
+            {
+
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+            
+            }
+        }
+    }
+  public void SetupGem(Vector2Int pos, Board theBoard)
+    {
+        posIndex = pos;
+        board = theBoard;
+    }
+
+    private void OnMouseDown()
+    {
+        if (board.currentState == Board.BoardState.move && board.roundMan.roundTime > 0)
+        {
+            if (board.currentState == Board.BoardState.move && board.roundMan.roundTime > 0)
+                firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePressed = true;
+        }
+    }
+
+    private void CalculateAngle()
+    {
+        SwipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        SwipeAngle = SwipeAngle * 180 / Mathf.PI;
+        Debug.Log(SwipeAngle);
+
+        if(Vector3.Distance(firstTouchPosition, finalTouchPosition)> .5f)
+        {
+            MovePieces();
+        }
+      
+    }
+
+    private void MovePieces()
+    {
+        previousPos = posIndex;
+
+        if (SwipeAngle < 45 && SwipeAngle > -45 && posIndex.x < board.width - 1)
+        {
+            if (posIndex.x + 1 == board.allGems.GetLength(0)) return;
+
+            otherGem = board.allGems[posIndex.x + 1, posIndex.y];
+            otherGem.posIndex.x--;
+            posIndex.x++;
+        }
+        else if (SwipeAngle > 45 && SwipeAngle <= 135 && posIndex.y < board.height - 1)
+        {
+            if (posIndex.y + 1 == board.allGems.GetLength(1)) return;
+
+            otherGem = board.allGems[posIndex.x, posIndex.y + 1];
+            otherGem.posIndex.y--;
+            posIndex.y++;
+        }
+        else if (SwipeAngle < -45 && SwipeAngle >= -135 && posIndex.y > 0)
+        {
+            if (posIndex.y == 0) return;
+
+            otherGem = board.allGems[posIndex.x, posIndex.y - 1];
+            otherGem.posIndex.y++;
+            posIndex.y--;
+        }
+        else if (SwipeAngle > 135 || SwipeAngle < -135 && posIndex.x > 0)
+        {
+            if (posIndex.x == 0) return;
+
+            otherGem = board.allGems[posIndex.x - 1, posIndex.y];
+            otherGem.posIndex.x++;
+            posIndex.x--;
+        }
+
+        if (otherGem == null) return;
+        board.allGems[posIndex.x, posIndex.y] = this;
+        board.allGems[otherGem.posIndex.x, otherGem.posIndex.y] = otherGem;
+
+        StartCoroutine(CheckMoveCo());
+    }  
+    
+    public IEnumerator CheckMoveCo()
+    {
+        board.currentState = Board.BoardState.wait;
+
+        yield return new WaitForSeconds(.5f);
+
+        board.matchFind.FindAllMatches();
+
+        if(otherGem != null)
+        {
+            if(!isMatched && !otherGem.isMatched)
+            {
+                otherGem.posIndex = posIndex;
+                posIndex = previousPos;
+
+                board.allGems[posIndex.x, posIndex.y] = this;
+                board.allGems[otherGem.posIndex.x, otherGem.posIndex.y] = otherGem;
+                board.allGems[otherGem.posIndex.x, otherGem.posIndex.y] = otherGem;
+
+                yield return new WaitForSeconds(.5f);
+
+                board.currentState = Board.BoardState.move;
+            } else
+            {
+                board.DestroyMatches();
+            }
+        }
+    }
+
+  }
+
